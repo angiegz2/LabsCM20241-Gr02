8ac07883094c9016dd36355ecfc9ca6b14bee204
LCarlosVZ <lcarlosvazap@gmail.com>
2024-03-16 18:35:07 -0500

    Se creó la clase ContactDataActivity, una lista de países y una lista de ciudades

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20241/lab1/ContactDataActivity.kt
@@ -0,0 +1,43 @@
+package co.edu.udea.compumovil.gr02_20241.lab1
+
+class ContactDataActivity : AppCompatActivity() {
+    val country = arrayOf(
+        "Argentina",
+        "Bolivia",
+        "Brasil",
+        "Chile",
+        "Colombia",
+        "Costa Rica",
+        "Cuba",
+        "Ecuador",
+        "El Salvador",
+        "Guatemala",
+        "Haití",
+        "Honduras",
+        "México",
+        "Nicaragua",
+        "Panamá",
+        "Paraguay",
+        "Perú",
+        "República Dominicana",
+        "Uruguay",
+        "Venezuela"
+    )
+    val city = arrayOf(
+        "Bogotá",
+        "Medellín",
+        "Cali",
+        "Bucaramanga",
+        "Cartagena",
+        "Soacha",
+        "Cúcuta",
+        "Soledad",
+        "Bucaramanga",
+        "Pereira",
+        "Montería",
+        "Pasto",
+        "Bello",
+        "Armenia",
+        "Villavicencio"
+    )
+}
