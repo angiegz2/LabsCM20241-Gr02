befaacfcc9f16479ddd83f34411e8b39e3b39739
angiegz2 <angie02yarce@gmail.com>
2024-03-16 18:51:45 -0500

    Creación y restricciónes para botones

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20241/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20241/lab1/PersonalDataActivity.kt
@@ -1,2 +1,29 @@
 package co.edu.udea.compumovil.gr02_20241.lab1
 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import androidx.appcompat.app.AppCompatActivity
+
+class PersonalDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_data)
+        setTitle(R.string.personal_information)
+
+        findViewById<EditText>(R.id.etDate).setOnClickListener() {
+            showDatePikerDialog()
+        }
+        findViewById<RadioButton>(R.id.radioButtonM).setOnClickListener() {
+            findViewById<RadioButton>(R.id.radioButtonF).isChecked = false
+        }
+        findViewById<RadioButton>(R.id.radioButtonF).setOnClickListener() {
+            findViewById<RadioButton>(R.id.radioButtonM).isChecked = false
+        }
+        findViewById<Button>(R.id.ButtonContacNext).setOnClickListener() {
+            personValidation()
+        }
+
+    }
+}
