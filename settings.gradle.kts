3932673f957df0afea7507cc83be5c935b15e76a
LCarlosVZ <lcarlosvazap@gmail.com>
2024-03-16 01:15:37 -0500

    Agregar archivos faltantes al proyecto base

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LabsCM20241-Gr02"
+include(":Lab1-UI")
+ 
