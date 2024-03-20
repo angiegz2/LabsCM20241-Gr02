5ab28725bb0797b4f6354bfb9a79747d40b5a7c7
LCarlosVZ <lcarlosvazap@gmail.com>
2024-03-19 21:23:12 -0500

    Se añadieron funciones a la clase ContactDataActivity

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20241/lab1/ContactDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20241/lab1/ContactDataActivity.kt
@@ -1,5 +1,13 @@
 package co.edu.udea.compumovil.gr02_20241.lab1
 
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+
 class ContactDataActivity : AppCompatActivity() {
     val country = arrayOf(
         "Argentina",
@@ -40,4 +48,33 @@ class ContactDataActivity : AppCompatActivity() {
         "Armenia",
         "Villavicencio"
     )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_data)
+        setTitle(R.string.Contact_information)
+        autoCompleteCountry()
+        autoCompleteCity()
+        findViewById<Button>(R.id.ButtonContacNext).setOnClickListener(){
+            contactValidation()
+        }
+    }
+
+    private fun autoCompleteCountry(){
+        val campoSugerencias = findViewById<View>(R.id.contactCity) as AutoCompleteTextView
+
+        val adaptador: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, city)
+
+        campoSugerencias.setAdapter(adaptador)
+    }
+    private fun autoCompleteCity(){
+        val campoSugerencias = findViewById<View>(R.id.contactCountry) as AutoCompleteTextView
+
+        val adaptador: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,country )
+
+        campoSugerencias.setAdapter(adaptador)
+    }
 }
+
